8b7c06e2bc3635133849085f649ccdec3a9bfc3f
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-09-13 07:35:33 -0400

    Initial commit of intellij project

---
+++ src/com/LickinHeights/Main.java
@@ -0,0 +1,49 @@
+package com.LickinHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    int firstNumber,secondNumber,total;
+
+int height;
+
+
+
+    firstNumber=5;
+    secondNumber=10;
+    total=firstNumber+secondNumber;
+    System.out.println(total);
+    total=firstNumber*25;
+    System.out.println(total);
+
+
+;
+
+
+   /* System.out.println("first number+second number is "+ (firstNumber+secondNumber ));
+        System.out.println("firstnumber + secondnumber "+ (secondNumber/firstNumber ));
+        System.out.println("firstNumbern + secondNumber " + (firstNumber-secondNumber));
+        System.out.println("firstNumber + seconNumber " + (secondNumber%firstNumber));
+        System.out.println(firstNumber*secondNumber);
+        System.out.println(total);
+        total=total+10;
+        System.out.println(total);
+        System.out.println("What is your height ?(inches only)");
+
+*/
+
+
+
+
+
+
+
+
+
+
+    }
+}
