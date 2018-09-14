cec3f9d0754a0444e1eb40ac05ea25cd8d95b0b3
xxLuckycoderxx <42833597+xxLuckycoderxx@users.noreply.github.com>
2018-09-14 07:59:02 -0400

    Update Main.java
    made changes to the first attempt

--- src/com/LickinHeights/Main.java
+++ src/com/LickinHeights/Main.java
@@ -7,45 +7,27 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-    int firstNumber,secondNumber,total;
-Scanner keyboard;
-String age;
-int height ;
-double inches;
+ 
+        int firstNumber;
+        int secondNumber;
+
+        //declare
+
+        firstNumber=5;
+        secondNumber=20;
 
 
-    firstNumber=5;
-    secondNumber=10;
-    total=firstNumber+secondNumber;
-    System.out.println(total);
-    total=firstNumber*25;
-    System.out.println(total);
-    keyboard=new Scanner(System.in);
-    inches=0.024/1;
+        System.out.println("first number added to the second number is " + (firstNumber+secondNumber));
+        System.out.println("first number subtracted from the second number is " + (firstNumber-secondNumber));
+        System.out.println("first number multiplied by the second number is " +(firstNumber*secondNumber));
+        System.out.println("first number divided by the second number is " +(firstNumber/secondNumber));
+        System.out.println("The modulus of the first number and second number is " +(firstNumber%secondNumber));
 
- // BmI=kg/m*m
-        System.out.println("What is your height in inches");
-        height=keyboard.nextInt();
-        System.out.println(height*inches);
-        System.out.println("What is your weight in kilograms");
+  
 
 
 
 
-
-
-
-
-   /* System.out.println("first number+second number is "+ (firstNumber+secondNumber ));
-        System.out.println("firstnumber + secondnumber "+ (secondNumber/firstNumber ));
-        System.out.println("firstNumbern + secondNumber " + (firstNumber-secondNumber));
-        System.out.println("firstNumber + seconNumber " + (secondNumber%firstNumber));
-        System.out.println(firstNumber*secondNumber);
-        System.out.println(total);
-        total=total+10;
-        System.out.println(total);
-        System.out.println("What is your height ?(inches only)");
-*/
 
 
 
